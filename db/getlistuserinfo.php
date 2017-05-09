dc763ad8e29c82b75326fbb9ada0121c3a55445a
MiracleDevelopment <i.patipan2539@gmail.com>
2017-05-09 13:35:07 +0700

    updateDB

--- db/getlistuserinfo.php
+++ db/getlistuserinfo.php
@@ -2,42 +2,47 @@
 require("connector.php");
 if($connection){
     $listUserInfo = [];
+    $listUserMember = [];
+    $listUserItem = [];
+
     $reponse =[];
-    $sqlAllQuery = "SELECT * From History";
+    $sqlAllQuery = "SELECT * FROM History_TB";
     $result = mysqli_query($connection,$sqlAllQuery);
     if(mysqli_num_rows($result)>0){
         while($rows = mysqli_fetch_assoc($result)){
-            $user=$rows["m_id"];
-            $itemId = $rows["i_id"];
+            //  $user=$rows["m_id"];
+            //  $itemId = $rows["i_id"];
             $listUserInfo["memberId"] = $rows["m_id"];
             $listUserInfo["itemId"]  = $rows["i_id"];
             $listUserInfo["count"] =$rows["count"];
-            
-           
+            $response[0] = $listUserInfo;
         }
         
         $sqlquery = "SELECT fistname,lastname,email FROM Member_TB WHERE member_id = '$user'";
         $resultinner = mysqli_query($connection,$sqlquery);
         if(mysqli_num_rows($resultinner)>0){
             while($rowsinner = mysqli_fetch_assoc($resultinner)){
-                $listUserInfo["firstname"] = $rows["firstname"];
-                $listUserInfo["lastname"] = $rows["lastname"];
-                $listUserInfo["email"] = $rows["email"];
+                $listUserMember["firstname"] = $rows["firstname"];
+                $listUserMember["lastname"] = $rows["lastname"];
+                $listUserMember["email"] = $rows["email"];
+                $response[1] = $listUserMember;
             }
         }
         
-
+        
         $sqlqueryThird = "SELECT price,item_name FROM Item_TB WHERE item_name = '$itemId'";
-        $conn = mysqli_query($connection,$sqlqueryThird);
+        $resultinnerconnection = mysqli_query($connection,$sqlqueryThird);
         if(mysqli_num_rows($connection,$sqlqueryThird)){
             while($row = mysqli_fetch_assoc($conn)){
-                    $listUserInfo["price"] = $rows["price"];
-                    $listUserInfo["name"]= $rows["item_name"];
+                $listUserItem["price"] = $rows["price"];
+                $listUserItem["name"]= $rows["item_name"];
+                $response[2] = $listUserItem;
             }
         }
-        $response[] = $listUserInfo;
+        
     }
     echo json_encode($reponse);
+    
 }
 
 
