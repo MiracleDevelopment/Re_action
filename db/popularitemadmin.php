b79829e56bd57f1225db5b7435b545d5244bca19
MiracleDevelopment <i.patipan2539@gmail.com>
2017-05-12 02:06:26 +0700

    load

--- db/popularitemadmin.php
+++ db/popularitemadmin.php
@@ -1,25 +1,33 @@
-<?php 
-    require("connector.php");
-    if($connection){
-        $info=[];
-        $type=[];
-        $num = $_POST["num"];
-        $path = $_POST["poppular"];
-        $query = "INSERT INTO Popular_TB(id_type,i_id) VALUES ('$num','$path')";
-        $result = mysqli_query($connection,$query);
-        if($result){
-            $type["type"] = "OK";
-            $info[] = $type;
-        }else{
-            $type["type"] = $path;
-            $info[]= $type;
-        }
-        echo json_encode($type);
-    }else{
-        $type["status"]  ="true";
-        $info[] = $type;
-        echo json_encode($info);
+<?php
+require("connector.php");
+if($connection){
+    $info=[];
+    $type=[];
+    $type = $_POST["type"];
+    $url = $_FILES['imagesurl']['tmp_name'];
+    
+    $file_name = $_FILES['imageurl']['name'];
+    $path = "image/".$file_name;
+    if(move_uploaded_file($url,"../image/".$file_name))
+    {
+        chmod("../image/".$file_name, 0777);
     }
+    
+    $query = "INSERT INTO Popular_TB(id_type,i_id) VALUES ('$type','$path')";
+    $result = mysqli_query($connection,$query);
+    if($result){
+        $type["type"] = "OK";
+        $info[] = $type;
+    }else{
+        $type["type"] = $path;
+        $info[]= $type;
+    }
+    echo json_encode($type);
+}else{
+    $type["status"]  ="true";
+    $info[] = $type;
+    echo json_encode($info);
+}
 
 
 ?>
