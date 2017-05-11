39160f8030a963a3502b0ad4e516031c0dc1d9c9
MiracleDevelopment <i.patipan2539@gmail.com>
2017-05-11 23:01:54 +0700

    updateUserCommand

--- db/usercommand.php
+++ db/usercommand.php
@@ -1,11 +1,49 @@
 <?php
 session_start();
-$msg = "Hello,it,me";
+
+if(isset($_SESSION["item"])){
+    $msg = $_SESSION["item"];
+}else{
+    $msg="";
+}
 $id = $_POST["id"];
-$item = $_POST["item"];
+
 $result = explode(",",$msg);
 $number = count($result);
+
+$have = false;
 for($i = 0 ;$i<$number;$i++){
-    print $result[$i];
+    $spliteText = explode("|",$result[$i]);
+    
+    if($spliteText[0]==$id){
+        $add = intval($spliteText[1]);
+        $add +=1;
+        $spliteText[1] = strval($add);
+        $have = true;
+        break;
+    }
 }
+
+if(!$have)
+{
+    if($msg == "")
+    {
+        $msg = $id . "|1";
+        
+    }
+    else
+    {
+        $msg += "," . $id . "|1";
+        
+    }
+}
+else
+{
+    $msg = implode(",", $result);
+    // echo $msg;
+    
+}
+
+$_SESSION["item"] = $msg;
+
 ?>
