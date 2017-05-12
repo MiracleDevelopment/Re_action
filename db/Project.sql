a512d2f6b754907caeb8f90bd61dd3c811e23e02
tstedjb04 <turkky.dnt@hotmail.com>
2017-05-13 02:29:27 +0700

    OK

--- db/Project.sql
+++ db/Project.sql
@@ -54,9 +54,6 @@ CREATE TABLE Banner_TB(
 -- -------------------------------
 -- ADD CONSTRIANT
 -- -------------------------------
-
-
-
 ALTER TABLE History_TB 
 ADD CONSTRAINT FOREIGN KEY his_mid_fk (m_id) REFERENCES Member_TB (member_id)
 ON UPDATE CASCADE 
@@ -81,14 +78,25 @@ INSERT Member_TB (member_id,firstname,lastname,username,password,address,email,p
 (7,"admin","admin","admin","admin","61/88","admin@gmail.com","02-900-9000");
 
 INSERT Item_TB (item_id,item_name, price, type,image_url) VALUES
-(1,"Garlic Bread", 100,"pasta", "image/pasta3.png"),
-(2,"Chicken Wings ",200,"pasta","image/pasta3.png"),
-(3,"Mushroom Spaghetti",150,"pasta","image/pasta3.png"),
-(4,"Bacon Topping",180,"promotion","image/pasta3.png"),
-(5,"Burger",100,"burger","image/pasta3.png"),
-(6,"Burger",200,"burger","image/pasta3.png"),
-(7,"Burger",300,"appetizers","image/pasta3.png"),
-(8,"Burger",400,"drink","image/pasta3.png");
+(1, "ชุดประหยัด set1", "สปาเก็ตตี้สไปซี่ซีฟู้ด ขนมปังกระเทียมชีส และมะนาวโซดา ในราคาที่ถูกกว่า", 199, "promotion", "image/promotion1.png"),
+(2, "ชุดประหยัด set2", "สปาเก็ตตี้ไวท์ครีมซอส ขนมปังกระเทียม และไก่นิวออร์ลีนส์ ในราคาที่ถูกกว่า", 199, "promotion", "image/promotion2.png"),
+
+(3, "สปาเก็ตตี้ไวท์ครีมซอส", "เนื้อที่ย่างบนเปลวไฟจนหอมกรุ่นถึง 2 ชั้น เติมความอร่อยด้วยเบคอนรมควัน กรุบกรอบด้วยผักสด", 149, "pasta", "image/pasta3.png"), 
+(4, "สปาเก็ตตี้สไปซี่ซีฟู้ด ", "สปาเก็ตตี้ซีฟู้ดรสชาติแบบไทยๆ จัดจ้านซอสผัดฉ่า เผ็ดถึงใจ", 139, "pasta", "image/pasta1.png"), 
+(5, "สปาเก็ตตี้ผัดฉ่า ไส้กรอกไก่", "สปาเก็ตตี้ไส้กรอกไก่ รสชาติแบบไทยๆ จัดจ้านซอสผัดฉ่า เผ็ดถึงใจ", 129, "pasta", "image/pasta5.png"), 
+
+(6, "วอปเปอร์", "เนื้อที่ย่างบนเปลวไฟจนหอมกรุ่นสุกกำลังดี ที่แสนจะเข้ากันกับผักสดใหม่ มะเขือเทศ หอมใหญ่ และแตงกวาดอง", 99, "burger", "image/burger1.png"), 
+(7, "มัชรูมสวิส", "เนื้อชิ้นหนา ย่างบนเปลวไฟหอมกรุ่น ราดด้วยเห็ดแชมปิญองชุ่มซอสสูตรพิเศษเฉพาะของเรา เสิร์ฟร้อนๆ บนสวิสชีสหอมกรุ่น", 99, "burger", "image/burger2.png"), 
+
+(8, "ขนมปังกระเทียม", "ขนมปังโรยงาเนื้อนุ่ม อบด้วยเนยกระเทียมหอมกรุ่น", 59, "appetizer", "image/a1.png"), 
+(9, "ขนมปังกระเทียมชีส", "พิเศษกับมอสซาเรลล่าชีสบนหน้าขนมปังกระเทียม เพิ่มความเข้มข้นหอมมันอีกระดับ", 69, "appetizer", "image/a2.png"), 
+(10, "ชีสซี่ ป๊อป", "แป้งนุ่มนุ่ม สอดไส้ชีสชั้นเลิศ… ชุปเกล็ดขนมปังกรุบกรอบ พร้อมโรยหน้าด้วยผงชีส เพิ่มรสชาติให้อร่อยยิ่งขึ้นด้วยชีสเต็มๆ", 59, "appetizer", "image/a3.png"),
+(11, "นักเก็ต", "นักเก็ตไก่เนื้อแน่นหมักเครื่องเทศหอมอร่อย จำนวน 6 ชิ้น เสิร์ฟพร้อมซอสบาร์บีคิวรสเข้มไม่เหมือนใคร", 99, "appetizer", "image/a4.png"),
+
+(12, "โค้ก/สไปรท์/แฟนต้า", "จับคู่มื้ออาหารกับเครื่องดื่มซาบซ่า จะเมนูไหนก็ลงตัว", 35, "drink", "image/d1.png"),
+(13, "ชามะนาวเย็น", "มาชาร์ตแบตเติมความสดชื่นด้วยชามะนาวเย็นๆ สักแก้ว กับรสชาติหอมหวาน", 35, "drink", "image/d2.png"),
+(14, "มะนาวโซดา", "เติมความสดชื่นด้วยมะนาวโซดา เติมความสดใสให้พร้อมสู้อากาศร้อนได้อีกด้วย", 65, "drink", "image/d3.png"),
+;
 
 INSERT History_TB(m_id,i_id,count,date_sold) VALUES
 (1,2,2,"2013-04-1"),
@@ -99,8 +107,8 @@ INSERT History_TB(m_id,i_id,count,date_sold) VALUES
 
 INSERT Popular_TB(id,imageurl) VALUES
 (1,"image/burger1.png"),
-(2,"image/burger1.png"),
-(3,"image/burger1.png");
+(2,"image/a1.png"),
+(3,"image/drink1.png");
 
 INSERT Banner_TB(b_id,b_imageurl) VALUES
 (1,"image/promotion.jpg"),
