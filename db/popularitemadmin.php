67ff702b7dec64e9bddb9b0b70fa24a92c4f95f1
tstedjb04 <turkky.dnt@hotmail.com>
2017-05-12 20:39:11 +0700

    add thank.html

--- db/popularitemadmin.php
+++ db/popularitemadmin.php
@@ -4,13 +4,15 @@ if($connection){
     $info=[];
     $type=[];
     $id= $_POST["id"];
-    $type = $_POST["type"];
+    //$type = $_POST["type"];
     $url = $_FILES['imageurl']['tmp_name'];
     
     $file_name = $_FILES['imageurl']['name'];
     $path = "image/".$file_name;
+    chmod("../image", 0777);
     if(move_uploaded_file($url,"../image/".$file_name))
     {
+        echo "Moved";
         chmod("../image/".$file_name, 0777);
     }
     echo "$id";
@@ -20,7 +22,7 @@ if($connection){
         $type["type"] = "OK";
         $info[] = $type;
     }else{
-        $type["type"] = $type;
+        $type["type"] = "NO";
         $type["url"]  = $url;
         $type["file_name"] = $file_name;
         $type["path"] = $path;
