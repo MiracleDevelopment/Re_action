f081d6bca05a5ca82ab7532a3f7b54249d393106
MiracleDevelopment <i.patipan2539@gmail.com>
2017-05-12 17:30:58 +0700

    updateGetBanner

---
+++ db/getbanner.php
@@ -0,0 +1,18 @@
+<?php
+require("connector.php");
+if($connection){
+    $info =[];
+    $response=[];
+    $query  = "SELECT * FROM Banner_TB";
+    $result = mysqli_query($connection,$query);
+    if(mysqli_num_rows($result)>0){
+        while($rows = mysqli_fetch_assoc($result)){
+            $info["id"] = $rows["b_id"];
+            $info["image"] = $rows["b_image"];
+            $response[]=$info;
+        }
+    }
+    echo json_encode($response);
+}
+
+?>
